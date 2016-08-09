c13a9647797898a5f01476d63a2a2dc855fc3ede
Daniel A. Maierhofer <git@damadmai.at>
2016-08-09 13:17:08 +0200

    Add KeyboardInterrupt

--- ser2file/ser2file.py
+++ ser2file/ser2file.py
@@ -6,7 +6,7 @@ import datetime
 if __name__=='__main__':
 	try:
 		ser = serial.Serial('COM1', 115200, timeout=1)
-		print("opened" + ser.portstr)
+		print("opened " + ser.portstr)
 	except serial.SerialException:
 		print('could not open port')
 		sys.exit(1)
@@ -14,11 +14,16 @@ if __name__=='__main__':
 	f = open(datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S") + '.csv', 'w+')
 	f.write('10ms;x;y;z;delta\n')
 	i = 0
-	while True:
-		line = ser.readline().decode('utf-8')
-		if line:
-			f.write(str(i) + ';' + line)
-			i = i + 1
-		print(line)
+	line = ser.readline().decode('utf-8') # Truncate first read line
+	try:
+		while True:
+			line = ser.readline().decode('utf-8')
+			if line:
+				f.write(str(i) + ';' + line)
+				i = i + 1
+			print(line)
+	except KeyboardInterrupt:
+		pass
+
 	f.close()
 	ser.close()
