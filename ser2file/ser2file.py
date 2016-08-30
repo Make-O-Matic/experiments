3aecab9a478f4925c1e9ac62b19cb9331be156ff
Daniel A. Maierhofer <git@damadmai.at>
2016-08-30 18:16:07 +0200

    Adapt for binary COBS input

--- ser2file/ser2file.py
+++ ser2file/ser2file.py
@@ -2,28 +2,42 @@
 import sys
 import serial
 import datetime
+from cobs import cobs
+import struct
+import csv
+import json
+
+csv_caption = ['rfid','ex','ey','ez','ax','ay','az','myo','keyswcaplastnr']
+def unpack_pkg(stream):
+	pkg = struct.unpack('12sffffffHB', stream)
+	return pkg
 
 if __name__=='__main__':
 	try:
-		ser = serial.Serial('COM1', 115200, timeout=1)
+		ser = serial.Serial('/dev/ttyUSB1', 115200, timeout=1)
 		print("opened " + ser.portstr)
 	except serial.SerialException:
 		print('could not open port')
 		sys.exit(1)
 		pass
-	f = open(datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S") + '.txt', 'w+')
-	f.write('10ms\tx\ty\tz\tdelta\n')
-	i = 0
-	line = ser.readline().decode('utf-8') # Truncate first read line
-	try:
-		while True:
-			line = ser.readline().decode('utf-8')
-			if line:
-				f.write(str(i) + '\t' + line.replace(';', '\t'))
-				i = i + 1
-			print(line)
-	except KeyboardInterrupt:
-		pass
-
-	f.close()
+	with open(datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S") + '.txt', 'w+') as f:
+		csv_f = csv.writer(f)
+		csv_f.writerow(csv_caption)
+		line = ""
+		try:
+			while True:
+				if ser.read() == '\0':
+					break
+			while True:
+				c = ser.read()
+				if c == '\0':
+					pkg = unpack_pkg(cobs.decode(line))
+					print(json.JSONEncoder().encode(pkg))
+					csv_f.writerow(pkg)
+					line = ""
+				else:
+					line += c
+		except KeyboardInterrupt:
+			pass
+		f.close()
 	ser.close()
